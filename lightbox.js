d4c3f67949e8c047ac29ffdf2b52d8cca7339bbe
savannahhendrickson11 <31701285+savannahhendrickson11@users.noreply.github.com>
2017-12-17 19:09:38 -0500

    Update lightbox.js

--- lightbox.js
+++ lightbox.js
@@ -134,6 +134,93 @@
   document.body.addEventListener('keyup', onKeyUp);
 
 }());
+    var galleryContent = document.querySelector('.gallery-content');
+    var oldImage = galleryContent.querySelector('img');
+    if (oldImage) {
+      galleryContent.removeChild(oldImage);
+    }
+  
+    galleryContent.appendChild(img);
+  
+    updateNavigation();
+  }
+
+  function getItemIndex(items, item) {
+    return Array.from(items).indexOf(item);
+  }
+
+  function onGalleryItemClick(event) {
+    var clickedGalleryItem = event.currentTarget;
+  
+    showGallery();
+    galleryItemIndex = getItemIndex(galleryItems, clickedGalleryItem);
+    showImage();
+  }
+
+  for(var i = 0; i < galleryItems.length; i++) {
+    galleryItems[i].addEventListener('click', onGalleryItemClick);
+  }
+
+  function onCloseButtonClick() {
+    hideGallery();
+  }
+
+  closeButton.addEventListener('click', onCloseButtonClick);
+
+  function onNextButtonClick() {
+    galleryItemIndex++;
+    if (galleryItemIndex === galleryItems.length) {
+      galleryItemIndex = 0;
+    }
+    showImage();
+  }
+
+  nextButton.addEventListener('click', onNextButtonClick);
+
+
+  function onPreviousButtonClick() {
+    galleryItemIndex--;
+    if (galleryItemIndex === -1) {
+      galleryItemIndex = galleryItems.length - 1;
+    }
+    showImage();
+  }
+
+  previousButton.addEventListener('click', onPreviousButtonClick);
+
+  function onNavigationButtonClick(event) {
+    var clickedNavigationItem = event.currentTarget;
+  
+    galleryItemIndex = getItemIndex(navItems, clickedNavigationItem);
+    showImage();
+  }
+
+  for(var i = 0; i < navItems.length; i++) {
+    navItems[i].addEventListener('click', onNavigationButtonClick);
+  }
+  
+  
+  function onKeyUp(event) {
+    if (event.which === 27) {
+      //Escape key up
+      hideGallery();
+      
+    } else if (event.which === 39) {
+      //Arrow right key up
+      onNextButtonClick();
+      
+    } else if (event.which === 37) {
+      //Arrow left key up
+      onPreviousButtonClick();
+      
+    }
+  }
+  
+  
+  
+  document.body.addEventListener('keyup', onKeyUp);
+
+}());
 
 
 
